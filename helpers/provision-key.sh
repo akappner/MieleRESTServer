a0218ff83fd19ca199dee5d90ca5b39f213de828
Alexander Kappner <alexander@kappner.info>
2026-01-06 20:25:51 -0800

    provision-key.sh: Add magic authorization header to enable pairing with certain additional devices (e.g. WWG760)

--- helpers/provision-key.sh
+++ helpers/provision-key.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 MIELEIP="$1"
 TARGETFILE="$2"
+
 echo "Trying HTTP"
 wget --tries=2 --timeout=3 --connect-timeout=3 -O - --method=PUT --body-file="$TARGETFILE" http://$MIELEIP/Security/Commissioning
 echo "Trying HTTPS"
-wget --tries 2 --timeout=3 --no-check-certificate -O - --method=PUT --body-file="$TARGETFILE" https://$MIELEIP/Security/Commissioning
+wget  --header="Authorization: MielePairing:Pairing" --tries 2 --timeout=3 --no-check-certificate -O - --method=PUT --body-file="$TARGETFILE" https://$MIELEIP/Security/Commissioning
